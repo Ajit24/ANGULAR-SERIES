8c76847ac84922721041e6d41d98f50534bfba40
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2022-12-15 09:56:36 +0530

    angular updates

--- day1/my-app/src/app/app.component.ts
+++ day1/my-app/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+// obj = {
+//   name:'ajit',
+//   age:'20'
+// }
+// arr = ['ajit', 'shakti', 'santu']
+currentVal = ""
+getVal(val)
+{
+  console.log(val)
+  this.currentVal = val
+}
+
+
 }
