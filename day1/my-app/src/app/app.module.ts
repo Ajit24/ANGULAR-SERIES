16966808d7df28c59d022c350b779378d04cd1eb
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2022-12-16 10:37:39 +0530

    loop NG

--- day1/my-app/src/app/app.module.ts
+++ day1/my-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
