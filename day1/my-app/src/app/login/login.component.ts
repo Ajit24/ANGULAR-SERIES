7b07626f747e540701b16b8c29ffd2e5d7e2ae03
Ajit Kumar Pandey <87422047+Ajit24@users.noreply.github.com>
2022-12-28 10:01:30 +0530

    LOGIN COMPONENT

--- day1/my-app/src/app/login/login.component.ts
+++ day1/my-app/src/app/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  signupUsers:any[]=[];
+  signupObj:any={
+    name:'',
+    email: '',
+    password:''
+  }
+
+  loginObj:any={
+    name:'',
+    password:''
+  }
+
+constructor(){}
+
+ngOnInit(): void {
+  
+}
+
+onSignup(){
+  this.signupUsers.push(this.signupObj);
+  localStorage.setItem('signupUsers', JSON.stringify(this.signupUsers))
+    }
+  
+    onLogin(){
+  
+    }
+  
 
 }
